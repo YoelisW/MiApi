bbdfed3297b0e9455ec979d0bd14b4f624b9e60b
YoelisW <oyoelismario@gmail.com>
2026-06-27 14:09:02 -0600

    Preparar despliegue en Render

--- NativaSodaAPI/Program.cs
+++ NativaSodaAPI/Program.cs
@@ -18,11 +18,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("PermitirTodo");
 
